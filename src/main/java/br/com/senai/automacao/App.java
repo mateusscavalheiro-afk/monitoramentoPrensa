da15bf0d2d0d8409c617f17e5b122c020ce3db8f
mateusscavalheiro-afk <mateus_s_cavalheiro@estudante.sesisenai.org.br>
2026-02-23 13:57:24 -0300

    firstcommit

--- src/main/java/br/com/senai/automacao/App.java
+++ src/main/java/br/com/senai/automacao/App.java
@@ -1,6 +1,6 @@
 package br.com.senai.automacao;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
---
+++ src/main/java/br/com/senai/automacao/LógicaNegócio.java
@@ -0,0 +1,4 @@
+package br.com.senai.automacao;
+
+public class LógicaNegócio {
+}
